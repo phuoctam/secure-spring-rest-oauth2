7491d76ad64efa729a5f59efe3b41b3dea655e22
TamNguyen <phuoctam33k@gmail.com>
2017-10-01 10:51:42 +0700

    first commit

---
+++ src/main/java/com/smd/springmvc/configuration/ProjectConfiguration.java
@@ -0,0 +1,13 @@
+package com.smd.springmvc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.smd.springmvc")
+public class ProjectConfiguration {
+	
+
+}
---
+++ src/main/java/com/smd/springmvc/service/UserService.java
@@ -0,0 +1,26 @@
+package com.smd.springmvc.service;
+
+import java.util.List;
+
+import com.smd.springmvc.model.User;
+
+
+public interface UserService {
+	
+	User findById(long id);
+	
+	User findByName(String name);
+	
+	void saveUser(User user);
+	
+	void updateUser(User user);
+	
+	void deleteUserById(long id);
+
+	List<User> findAllUsers(); 
+	
+	void deleteAllUsers();
+	
+	public boolean isUserExist(User user);
+	
+}
